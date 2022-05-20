4c5db7d4cdc2e8aae78ad076bc3dc1a652a33f75
SZF1 A2 <szf1_a2@oktatas.local>
2022-05-20 12:21:40 +0200

    Hajo peldanyositasa tesztLoves metodusban, tesztLoves meghivasa main-ben

--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -1,19 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package main;
 
-/**
- *
- * @author szf1_a2
- */
+
 public class TorpedoTeszt {
-    public static void main(String[] args) {
-        
+    private Hajo hajo;
+    public void main(String[] args) {
+        tesztLoves(0);
     }
-    public static String tesztLoves(int poz){
+    public String tesztLoves(int poz){
+        hajo= new Hajo();
+        String t = hajo.talalat(4);
+        assert t.equals("talált") : "nem jó a találat ellenőrzése";
         return "";
     }
 }
