1bc133249b343d81925d3dca5ade8910dc61dc79
SZF1 A2 <szf1_a2@oktatas.local>
2022-05-20 12:12:49 +0200

    TorpedoTeszt

---
+++ src/main/TorpedoTeszt.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author szf1_a2
+ */
+public class TorpedoTeszt {
+    public static void main(String[] args) {
+        
+    }
+    public static String tesztLoves(int poz){
+        return "";
+    }
+}
