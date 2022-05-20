434997bba6b25e12a0ca3d04b40d6fa7d60dc94d
SZF1 A2 <szf1_a2@oktatas.local>
2022-05-20 12:14:36 +0200

    Hajo osztaly

---
+++ src/main/Hajo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author szf1_a2
+ */
+public class Hajo {
+    private int[] pozicio;
+
+    public Hajo(int[] pozicio) {
+        this.pozicio = new int[3];
+    }
+    
+    
+    public String talalat(int poz){
+        return "";
+    }
+}
